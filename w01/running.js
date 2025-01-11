ebabed85d992e14e746a6b1d8359f5c10ddaf86b
VictorB <anzac62@gmail.com>
2025-01-11 12:48:53 -0700

    week01

---
+++ w01/running.js
@@ -0,0 +1,5 @@
+// running.js
+const apples = 5;
+const oranges = 3;
+let total = apples + oranges;
+console.log("total:", total);
---
+++ w01/running2.js
@@ -0,0 +1,8 @@
+// Javascript file
+const myApples = 4;
+const friendApples = 2;
+let total = myApples + friendApples;
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
